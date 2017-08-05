5aed9fe332ec4ee8ea56ae94abce8eb4754ecf8e
victoria echevarria <viclee953@gmail.com>
2017-08-05 16:47:03 -0400

    second commit

---
+++ lib1/js/biography.js
@@ -0,0 +1,20 @@
+function validate() {
+
+if(fname.length === 0) {
+  alert("First Name is not filled out. Please enter your first name.");
+}
+
+if(lname.length === 0) {
+  alert("Last Name is not filled out. Please enter your last name");
+}
+
+if(email.length !=== /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/) {
+  alert("Please enter a valid email.")
+}
+
+if(phone.length !=== /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/) {
+  alert("Please enter a valid phone number.")
+}
+}
+
+validate();
