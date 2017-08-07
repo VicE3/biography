804c598bc152ff48168a8f66cb07f446a37cbd4a
victoria echevarria <viclee953@gmail.com>
2017-08-06 21:18:34 -0400

    finally

--- lib1/js/biography.js
+++ lib1/js/biography.js
@@ -1,12 +1,13 @@
 
-form.addEventListener(onsubmit, validate());
-
-
-
-
 function validate() {
+//Setting html first name, last name, email, and phone equal to variables so that I can refer to them in java script.
+//This says look at the html form named 'formval', set the value of the field (fname lname email and phone) on that form equal to var.
+var  fname = document.forms['formval'] ['fname'].value;
+var  lname = document.forms['formval'] ['lname'].value;
+var  email = document.forms['formval'] ['email'].value;
+var  phone = document.forms['formval'] ['phone'].value;
 
-if(fname.length === 0) {
+if(fname === "") {
   alert("First Name is not filled out. Please enter your first name.");
 }
 
@@ -14,11 +15,12 @@ if(lname.length === 0) {
   alert("Last Name is not filled out. Please enter your last name");
 }
 
-if(email.length !=== /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/) {
+if(email.length !== /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/) {
   alert("Please enter a valid email.")
 }
 
-if(phone.length !=== /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/) {
+if(phone.length !== /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/) {
   alert("Please enter a valid phone number.")
 }
+
 }
